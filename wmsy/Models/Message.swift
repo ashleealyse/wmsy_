08c2dae2273754706c02fae9c18fb4fbe83bac57
Ashlee Krammer <ashlee.alyse@me.com>
2018-03-16 17:06:59 -0400

    Add Message Model

--- wmsy/Models/Message.swift
+++ wmsy/Models/Message.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+enum MessageType: String, Codable {
+    case chat
+    case notification
+    case button
+}
+
+struct Message: Codable {
+    let whimID: String
+    let messageID: String
+    let senderID: String?
+    let timestamp: Int
+    let messageType: MessageType
+    let messageBody: String
+    
+static let singleMessage = Message.init(whimID: "GPL13D5", messageID: "H923BB4", senderID: "TT09SC43", timestamp: 45378542, messageType: .chat, messageBody: "Yay Ice Cream")
+}
