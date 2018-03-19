ca121a4758c83011b3c2f80d8cd30419acc571da
Ashlee Krammer <ashlee.alyse@me.com>
2018-03-19 17:00:05 -0400

    Update Table View with Expanding Cells

---
+++ wmsy/Pages/FeedMapPage/FeedCell.swift
@@ -0,0 +1,72 @@
+//
+//  FeedCell.swift
+//  wmsy
+//
+//  Created by Ashlee Krammer on 3/19/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    var isExpanded = false {
+        didSet {
+            expandedConstraints.forEach{$0.isActive = isExpanded}
+            collapsedConstraints.forEach{$0.isActive = !isExpanded}
+            expandedView.isHidden = !isExpanded
+            collapsedView.isHidden = isExpanded
+        }
+    }
+    var collapsedView = CollapsedFeedCellView()
+    var expandedView = ExpandedFeedCellView()
+    var collapsedConstraints = [NSLayoutConstraint]()
+    var expandedConstraints = [NSLayoutConstraint]()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        commonInit()
+    }
+    
+    private func commonInit() {
+        backgroundColor = .white
+        setUpCollapsedView()
+        setUpExpandedView()
+        collapsedConstraints.forEach{$0.isActive = true}
+        expandedView.isHidden = true
+        selectionStyle = .none
+    }
+    
+    private func setUpCollapsedView() {
+        contentView.addSubview(collapsedView)
+        collapsedView.translatesAutoresizingMaskIntoConstraints = false
+        collapsedConstraints = [collapsedView.topAnchor.constraint(equalTo: contentView.topAnchor),
+                                collapsedView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+                                collapsedView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+                                collapsedView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+         ]
+        
+    }
+    
+    private func setUpExpandedView() {
+        contentView.addSubview(expandedView)
+        expandedView.translatesAutoresizingMaskIntoConstraints = false
+        expandedConstraints = [expandedView.topAnchor.constraint(equalTo: contentView.topAnchor),
+                               expandedView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+                               expandedView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+                               expandedView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ]
+        
+    }
+    
+}
--- wmsy/Pages/FeedMapPage/FeedMapVC.swift
+++ wmsy/Pages/FeedMapPage/FeedMapVC.swift
@@ -9,19 +9,25 @@
 import UIKit
 
 class FeedMapVC: UIViewController {
-
+    
+    var feedView = FeedView()
+    var expandedRows = Set<Int>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        view.addSubview(feedView)
+        feedView.tableView.register(FeedCell.self, forCellReuseIdentifier: "WhimFeedCell")
+        feedView.tableView.dataSource = self
+        feedView.tableView.delegate = self
+        feedView.tableView.rowHeight = UITableViewAutomaticDimension
+        feedView.tableView.estimatedRowHeight = 90
         configureNavBar()
     }
-
+    
     
     // setup UIBarButtonItem
     private func configureNavBar() {
-//        navigationItem.title = "wmsy"
+        //        navigationItem.title = "wmsy"
         // top left bar button item to Host a Whim (popsicles with a + symbol?)
         let topLeftBarItem = UIBarButtonItem(image: #imageLiteral(resourceName: "wmsyCategoryIcon"), style: .plain, target: self, action: #selector(hostAWhim))
         navigationItem.leftBarButtonItem = topLeftBarItem
@@ -39,15 +45,50 @@ class FeedMapVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension FeedMapVC: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) as? FeedCell else {
+                return
+        }
+        
+        switch cell.isExpanded {
+        case true:
+            self.expandedRows.remove(indexPath.row)
+        default:
+            self.expandedRows.insert(indexPath.row)
+        }
+        
+        cell.isExpanded = !cell.isExpanded
+        
+        tableView.beginUpdates()
+        tableView.endUpdates()
     }
-    */
+}
 
+extension FeedMapVC: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WhimFeedCell", for: indexPath) as! FeedCell
+        cell.isExpanded = self.expandedRows.contains(indexPath.row)
+        
+        return cell
+    }
 }
