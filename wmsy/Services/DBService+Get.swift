b210be791abd3d1bdc0733e95c3dc67acb2cad63
Margaret Chan <margaret.helen.chan@gmail.com>
2018-03-20 12:50:35 -0400

    add GetWhim functions

---
+++ wmsy/Services/DBService+Get.swift
@@ -0,0 +1,57 @@
+//
+//  DBService+Get.swift
+//  wmsy
+//
+//  Created by C4Q on 3/20/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+extension DBService {
+    
+    
+    
+    // GET ALL WHIMS
+    public func getAllWhims(completion: @escaping (_ whims: [Whim]) -> Void) {
+        whimsRef.observeSingleEvent(of: .value) { (dataSnapshot) in
+            var whims: [Whim] = []
+            
+            guard let whimSnapshots = dataSnapshot.children.allObjects as? [DataSnapshot] else { return }
+            for whimSnapshot in whimSnapshots {
+                guard let whimDict = whimSnapshot.value as? [String: Any] else { return }
+                guard
+                let id = whimDict["id"] as? String,
+                let category = whimDict["category"] as? String,
+                let title = whimDict["title"] as? String,
+                let description = whimDict["description"] as? String,
+                let hostID = whimDict["title"] as? String,
+                let location = whimDict["title"] as? String,
+                let duration = whimDict["title"] as? Int,
+                let finalized = whimDict["title"] as? Bool,
+                let timestamp = whimDict["title"] as? String,
+                let whimChats = whimDict["title"] as? [Message]
+                    else {
+                        print("Couldn't get post")
+                        return
+                }
+                let whim = Whim(id: id, category: category, title: title, description: description, hostID: hostID, location: location, duration: duration, finalized: finalized, timestamp: timestamp, whimChats: whimChats)
+            }
+            completion(whims.sortedByTimestamp())
+        }
+    
+    }
+    
+    
+    // GET SUBSET OF WHIMS BASED ON CATEGORY
+    public func getCategoryWhims(fromCategory category: String, completion: @escaping ([Whim]) -> Void) {
+        getAllWhims { (whims) in
+            let categoryWhims = whims.filter{$0.category == category}
+            completion(categoryWhims.sortedByTimestamp())
+        }
+    }
+    
+    
+    
+}
