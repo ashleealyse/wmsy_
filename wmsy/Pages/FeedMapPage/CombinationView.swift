d0d7436216753bb92773f9b566e5128232e53dde
Margaret Chan <margaret.helen.chan@gmail.com>
2018-03-22 13:24:56 -0400

    add combination view

---
+++ wmsy/Pages/FeedMapPage/CombinationView.swift
@@ -0,0 +1,19 @@
+//
+//  CombinationView.swift
+//  wmsy
+//
+//  Created by C4Q on 3/22/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class CombinationView: UIView {
+
+    // embed Feed View as base
+    
+    // create a scroll view with the mapview at the bottom of it, showing only the Mapview controls.
+    
+    // map view has a view with controls and a view with map
+
+}
