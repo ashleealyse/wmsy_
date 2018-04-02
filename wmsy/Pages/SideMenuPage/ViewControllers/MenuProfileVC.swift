c3711f7389fa519cb144806ef6daa92a399287fc
Margaret Chan <margaret.helen.chan@gmail.com>
2018-04-02 19:29:30 -0400

    replace Edit Bio textview handling with Alert

--- wmsy/Pages/SideMenuPage/ViewControllers/MenuProfileVC.swift
+++ wmsy/Pages/SideMenuPage/ViewControllers/MenuProfileVC.swift
@@ -12,14 +12,43 @@ import SnapKit
 class MenuProfileVC: UIViewController {
     
     let profileView = MenuProfileView()
+    private let placeholderText = "Describe Yourself"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(profileView)
+        
+        //        profileView.bioTextView.textColor = UIColor.lightGray
+        //        profileView.bioTextView.delegate = self
         profileView.snp.makeConstraints { (make) in
             make.edges.equalTo(view)
         }
+        
+        profileView.editBioButton.addTarget(self, action: #selector(editBio), for: .touchUpInside)
     }
+    @objc func editBio() {
+        let editBioAlert = Alert.create(withTitle: "Edit My Bio", andMessage: "", withPreferredStyle: .alert)
+        editBioAlert.addTextField { (textField: UITextField) in
+            textField.autocorrectionType = .default
+            textField.keyboardType = .default
+            textField.placeholder = "Tell us about yourself"
+            textField.clearButtonMode = .whileEditing
+            textField.text = self.profileView.bioTextView.text
+        }
+        Alert.addAction(withTitle: "Done", style: .default, andHandler: { (action) -> Void in
+            let textField = editBioAlert.textFields![0]
+            self.profileView.bioTextView.text = textField.text
+            DBService.manager.updateBio(textField.text!, forUser: AppUser.currentAppUser!)
+            self.profileView.bioTextView.resignFirstResponder()
+        }, to: editBioAlert)
+        Alert.addAction(withTitle: "Cancel", style: .cancel, andHandler: nil, to: editBioAlert)
+        self.present(editBioAlert, animated: true, completion: nil)
+    }
+
+    
+    
+    
+    
     
     public func configureWith(appUser: AppUser) {
         guard let url = URL(string: appUser.photoID) else {return}
