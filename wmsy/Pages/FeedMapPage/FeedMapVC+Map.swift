517f269c3d19adc0f6dcb6e920319eb87e9cd510
Alan Holguin <alanjholguin@gmail.com>
2018-04-03 16:26:21 -0400

    fixed table

--- wmsy/Pages/FeedMapPage/FeedMapVC+Map.swift
+++ wmsy/Pages/FeedMapPage/FeedMapVC+Map.swift
@@ -40,16 +40,18 @@ extension FeedMapVC: GMSMapViewDelegate{
         DBService.manager.getAppUser(fromID: hostID!) { (appUser) in
             self.currentUser = appUser
         }
-        self.mapView.detailView.userPicture.kf.setImage(with: hostURL, for: .normal)
-        let interests = getInterestKeys(appUser: AppUser.currentAppUser!)
-        if interests.contains(whimID!){
-            self.mapView.detailView.interestedButton.setImage(#imageLiteral(resourceName: "interestedCircleIcon"), for: .normal)
-        }else{
-            self.mapView.detailView.interestedButton.setImage(#imageLiteral(resourceName: "uninterestedCircleIcon"), for: .normal)
+        self.mapView.detailView.userPicture.kf.setImage(with: hostURL, for: .normal, placeholder: nil, options: nil, progressBlock: nil) { (image, error, cache, url) in
+            self.mapView.detailView.userPicture.imageView?.setNeedsDisplay()
+            let interests = self.getInterestKeys(appUser: AppUser.currentAppUser!)
+            if interests.contains(whimID!){
+                self.mapView.detailView.interestedButton.setImage(#imageLiteral(resourceName: "interestedCircleIcon"), for: .normal)
+            }else{
+                self.mapView.detailView.interestedButton.setImage(#imageLiteral(resourceName: "uninterestedCircleIcon"), for: .normal)
+            }
+            
+            self.mapView.detailView.isHidden = false
+            
         }
-        
-        self.mapView.detailView.isHidden = false
-
         return true
     }
 
