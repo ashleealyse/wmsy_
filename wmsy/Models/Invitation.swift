202b97dc5dc414bfbfc893b7773eb825386909a7
Lynk <lynk@Lynks-MacBook-Pro.local>
2019-12-03 13:55:45 -0500

    switched to empty project for Swift 5

--- wmsy/Models/Invitation.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Invitation.swift
-//  wmsy
-//
-//  Created by C4Q on 3/15/18.
-//  Copyright © 2018 C4Q. All rights reserved.
-//
-
-import Foundation
---
+++ wmsy_/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wmsy_
+//
+//  Created by Lynk on 12/3/19.
+//  Copyright © 2019 Lynk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
